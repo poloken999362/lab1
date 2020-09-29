522ac483ff4d92a0f169b1045a325130fa1c873b
yafu <yafu@LAPTOP-KC95B1AN>
2020-09-29 09:40:51 +0800

    D0588502 hw1

---
+++ src/lab1/Main.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm yeh-fu sun");
+		System.out.println("My nid is D0588502	");
+		System.out.println("This is my lab 1 hw");
+	}
+
+}
